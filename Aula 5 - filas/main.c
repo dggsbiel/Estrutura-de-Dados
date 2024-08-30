74948378ed1d2b8d334a65324bfa437301253036
dggsbiel <142501842+dggsbiel@users.noreply.github.com>
2024-08-30 13:45:34 +0000

    Aulas

---
+++ Aula 5 - filas/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void criafila( )
+
+int main(){
+
+    int fila;
+    int ip; // identificador do primeiro
+    int iu; // identificador do ultimo
+    int tamanhofila;
+    cria fila(int )
+
+    return 0;
+}
